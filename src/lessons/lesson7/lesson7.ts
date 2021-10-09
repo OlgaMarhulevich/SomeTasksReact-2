d213a543484ce93253bef0491b5a1ca67ad588ad
OlgaMarhulevich <omarhulevich@yandex.by>
2021-10-09 18:57:05 +0300

    15

--- src/lessons/lesson7/lesson7.ts
+++ src/lessons/lesson7/lesson7.ts
@@ -19,19 +19,61 @@ console.log('Lesson 7');
 // метода walk, eat, sleep - каждый метод должен выводить в консоль строку имя + действие. Пример:
 // walk => `${this.name} walking`
 // проверить, что методы работают
-
+class Animal {
+    constructor(public name: string = 'Animal') {
+    }
+    walk(){
+        return `${this.name} walking`
+    }
+    eat(){
+        return `${this.name} eating`
+    }
+    sleep(){
+        return `${this.name} sleeping`
+    }
+}
+let rabbit = new Animal('Rabbit')
+console.log(rabbit.eat())
+console.log(rabbit.walk())
+console.log(rabbit.sleep())
 
 //Task 02
 // Реализовать класс Monkey на базе класса Animal,  конструктор принимает name(по умолчанию 'Monkey') в качестве
 // параметра, реализовать методы roar и climb аналогично классу Animal
 // проверить, что все методы работают
-
+class Monkey extends Animal{
+    constructor(public name: string = 'Monkey') {
+        super(name);
+    }
+    roar(){
+        return `${this.name} roaring`
+    }
+    climb(){
+        return `${this.name} climbing`
+    }
+}
+let monkey = new Monkey()
+console.log(monkey.roar())
+console.log(monkey.climb())
 
 //Task 03
 // Реализовать класс Human на базе класса Monkey, конструктор принимает name(по умолчанию 'Human') в качестве
 // параметра, реализовать методы speak и think аналогично классу Animal
 // проверить, что все методы работают
-
+class Human extends Monkey{
+    constructor(public name: string = 'Human') {
+        super(name);
+    }
+    speak(){
+        return `${this.name} speaking`
+    }
+    think(){
+        return `${this.name} thinking`
+    }
+}
+let human = new Human()
+console.log(human.speak())
+console.log(human.think())
 
 // Task 04
 // Реализовать таски 01-03 через функции конструкторы в отдельном JS файле, реализовать наследование
